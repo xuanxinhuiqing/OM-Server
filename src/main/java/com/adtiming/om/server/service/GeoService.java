5edd57acbb3c53cafc9518d2b646fba9162c319d
aaron.song <jingshui007@hotmail.com>
2020-10-06 07:32:15 +0800

    test

--- src/main/java/com/adtiming/om/server/service/GeoService.java
+++ src/main/java/com/adtiming/om/server/service/GeoService.java
@@ -100,6 +100,7 @@ public class GeoService {
     }
 
     private String getClientIP(HttpServletRequest req) {
+    	LOG.debug("aaron.song ip Addr -> " + getIpAddr(req));
         String clientIP = req.getHeader("X-Real-IP");
         if (clientIP == null) {
             clientIP = req.getRemoteAddr();
@@ -111,6 +112,43 @@ public class GeoService {
 //        } else
 //            return remote_ip;
     }
+    
+    // ADDCODE aaron.song
+    /** 
+     * 获取当前网络ip 
+     * @param request 
+     * @return 
+     */  
+    public String getIpAddr(HttpServletRequest request){  
+        String ipAddress = request.getHeader("x-forwarded-for");  
+            if(ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {  
+                ipAddress = request.getHeader("Proxy-Client-IP");  
+            }  
+            if(ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {  
+                ipAddress = request.getHeader("WL-Proxy-Client-IP");  
+            }  
+            if(ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {  
+                ipAddress = request.getRemoteAddr();  
+                if(ipAddress.equals("127.0.0.1") || ipAddress.equals("0:0:0:0:0:0:0:1")){  
+                    //根据网卡取本机配置的IP  
+                    InetAddress inet=null;  
+                    try {  
+                        inet = InetAddress.getLocalHost();  
+                    } catch (UnknownHostException e) {  
+                        e.printStackTrace();  
+                    }  
+                    ipAddress= inet.getHostAddress();  
+                }  
+            }  
+            //对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割  
+            if(ipAddress!=null && ipAddress.length()>15){ //"***.***.***.***".length() = 15  
+                if(ipAddress.indexOf(",")>0){  
+                    ipAddress = ipAddress.substring(0,ipAddress.indexOf(","));  
+                }  
+            }  
+            return ipAddress;   
+    }
+    // 
 
     private String getCountry(AbstractCountryResponse res) {
         if (res == null)
